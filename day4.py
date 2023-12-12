ef7d52a3df8cf5cc686a35510e0c44c49cfee9ae
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-12-13 00:05:36 +0530

    done

--- day4.py
+++ day4.py
@@ -21,3 +21,26 @@ def find_lowest_number(secret_key):
 ans = find_lowest_number("iwrupvqb")
 
 print(ans)
+
+
+def find_lowest_number_six(secret_key, num_zeroes):
+    number = 1
+    target_prefix = "0" * num_zeroes
+    while True:
+        # Combine the secret key and the current number
+        input_string = f"{secret_key}{number}"
+
+        # Calculate the MD5 hash
+        md5_hash = hashlib.md5(input_string.encode()).hexdigest()
+
+        # Check if the hash has the desired number of leading zeroes
+        if md5_hash[:num_zeroes] == target_prefix:
+            return number
+
+        # Increment the number for the next iteration
+        number += 1
+
+
+ans2 = find_lowest_number_six("iwrupvqb", 6)
+
+print(ans2)
